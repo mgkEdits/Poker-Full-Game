0b440ade4b7083d82e12046f262910d95a011b56
Suleiman <suleosman73@gmail.com>
2024-01-28 19:19:26 +0300

    Added username to navBar

--- client/src/components/game/navBar.js
+++ client/src/components/game/navBar.js
@@ -1,17 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
-function navBar() {
+function NavBar() {
+  const [username, setUsername] = useState('');
+
+  // Use useEffect to update the username when the component mounts
+  useEffect(() => {
+    // Retrieve the username from local storage or an authentication context
+    const storedUsername = localStorage.getItem('username');
+    setUsername(storedUsername || 'Guest'); // Default to 'Guest' if no username is found
+  }, []);
+
   return (
     <div className='navBar-crd'>
-    <div className='left-item'>
-      <h1>Poker Game</h1>
-    </div>
+      <div className='left-item'>
+        <h1>Poker Game</h1>
+      </div>
 
-    <div className='right-item'>
-      <h2>Username</h2>
+      <div className='right-item'>
+        <h2>{username}</h2>
+      </div>
     </div>
-    </div>
-  )
+  );
 }
 
-export default navBar
+export default NavBar;
